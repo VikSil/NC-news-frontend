8c60de9f039cecaae3c0ecf4a3da8ee346aff580
VikSil <viktoria.sudraba@gmail.com>
2024-02-06 00:16:36 +0000

    Improve mobile layout of ArticleCard

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -8,12 +8,12 @@ export default function ArticleCard(props){
         <div className="d-flex flex-column w-100 native-border my-3 mx-4 py-3 px-4 ">
             <p className="text-end author">{article.author}</p>
             <h3 className="text-center mb-4">{article.title}</h3>
-            <div className="d-flex flex-row">
-                <p className="flex-grow-1 m-0 align-self-end">{created}</p>
-  
-                        <IconAndCount type = "likes" count = {article.votes}/>
-                        <IconAndCount type = "comments" count = {article.comment_count}/>
-
+            <div className="d-flex flex-row flex-wrap">
+                <p className="mb-0 me-auto align-self-end">{created}</p>
+                <div>
+                    <IconAndCount type = "likes" count = {article.votes}/>
+                    <IconAndCount type = "comments" count = {article.comment_count}/>
+                </div>
             </div>
         </div>
         
